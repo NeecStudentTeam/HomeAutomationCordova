2e14f0f618c4d23585a1a25dd567a29c8ee24e2b
TamuraShinnosuke <opwim04front@gmail.com>
2017-01-25 09:58:06 +0900

    second commit

--- HomeAutomation02/platforms/android/assets/www/scripts/index.js
+++ HomeAutomation02/platforms/android/assets/www/scripts/index.js
@@ -2,10 +2,15 @@
     'use strict'
 
     angular.module('my-app', ['onsen']);
+    angular.module('my-app').config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;application/json;charset=utf-8';
+
+    }]);
     angular.module('my-app').controller('MainController', ['$scope', '$http', function ($scope, $http) {
         $scope.init = function () {
-            $scope.detail = [{ name: 'hogehoge' }];
+            $scope.detail = [{ name: 'hogehoge', onoff: '' }];
         }
+
 
         $scope.onclick = function (num) {
             var pass = num;
@@ -20,7 +25,7 @@
             .error(function () {
             });
         }
-        function click() {
+        function getClick() {
             var url = 'https://api.loctouch.com/v1/railway/stations';
             $http({
                 method: 'JSONP',
@@ -35,19 +40,48 @@
             });
         }
 
+
         $scope.numIndex;
         $scope.detailClick = function (i) {
             $scope.numIndex = i;
             myModal.show();
         }
 
-        $scope.settingTest = function (i, set) {
-            $scope.detail.splice(i, 1, { name: set });
+        $scope.putClick = function () {
+            $scope.detail.splice($scope.numIndex, 1, { name: $scope.startCE, onoff: $scope.onOffName });
+            myModal.hide();
         }
+
+        $scope.deleteClick = function () {
+            var url = 'http://localhost/api/appliances/';
+            $http({
+                method: 'POST',
+                url: url,
+                data: { "name": "家電テスト2" }
+            })
+            .success(function (headers) {
+                alert('status:' + headers);
+            })
+            .error(function () {
+                alert('APIデータ送信失敗');
+            });
+        }
+
+        $scope.startCE;
+        $scope.settingTest = function (i) {
+            $scope.startCE = i;
+        }
+
+        $scope.onOffName;
+        $scope.onOffShow = function (j) {
+            $scope.onOffName = j;
+        }
+
 
         $scope.api = function () {
             myNavigator.pushPage('setting.html');
-            click();
+            getClick();
         }
     }]);
 })();
+
